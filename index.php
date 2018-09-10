0678c815fe34f5a4267b270b14ba4bc627060ecf
Michael McCulloch <m.mcculloch@gmail.com>
2018-09-09 21:55:34 -0400

    Added: index.php.  This file can currently be run to demonstrate the functionality of DataObjectInterface.php, DataSource.php, TextDB.php, and WpDataSource.php.

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @date 9/9/18
+ * @author Michael McCulloch
+ */
+
+// Tells php to look in the the same directory this file is in
+// for invoked classes.
+spl_autoload_register(
+  function ($className) {
+    include $className . '.php';
+  }
+);
+
+// Creates a data source by providing a url to get data from
+// and providing a location to store the relevant information.
+$dataSource = WpDataSource::create(
+  array(
+    'path' => 'posts.txt',
+    'url' => 'https://newsandfeatures.uncg.edu/wp-json/wp/v2/posts?_embed'
+  )
+);
+
+// Creates a queriable data object.
+$data = TextDB::create(
+  array(
+    'path' => $dataSource->getPath(),
+    'keys' =>   array( 'id', 'title', 'img_url', 'datetime', 'active')
+  )
+);
+
+// The following will soon be removed.
+// Note: var_dump($blah) will dump the value of $blah.
+
+// This is an example of how to get a record with a given id.
+var_dump($data->fetchById(36329));
+
+// This is an example of how to get all the records.
+var_dump($data->fetchAll());
+
+?>
