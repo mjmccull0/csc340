8925144d01559adf9bacf359ae3a2794b88209df
Michael McCulloch <m.mcculloch@gmail.com>
2018-09-18 15:04:40 -0400

    Added a function to Util.php which will trigger a controller and a controller action based on the requested url.

--- Util.php
+++ Util.php
@@ -24,5 +24,57 @@ class Util {
 
       return $fields;
   }
+
+
+  /**
+   * Takes a relative url and returns the appropriate 
+   * controller and action.
+   * @param string $_path relative url path. 
+   */
+  public static function getRoute($_path) {
+    $_path = trim($_path, "/");
+
+    // If the provided path does not indicate a 
+    // controller use the IndexController.
+    if (empty($_path)) {
+      $controller = new IndexController();
+      return $controller->indexAction();
+    }
+
+    // Create an array from the provided relative url path.
+    $path = explode("/", $_path);
+
+    // The first element of the array should be the name of a 
+    // controller.
+    $controllerName = ucfirst(array_shift($path)) . 'Controller';
+
+    if (!empty($path)) {
+      $actionName = array_shift($path) . 'Action';
+    } else {
+      $actionName = "indexAction";
+    }
+    
+    if (!empty($path)) {
+      $params = array_shift($path);
+    } else {
+      $params = array();
+    }
+
+    // Create an instance of the requested controller,
+    // if it exists.
+    if (class_exists($controllerName)) {
+      $controller = new $controllerName($params);
+    }
+
+    // Return the requested action, if it exists.
+    if (method_exists($controller, $actionName)) {
+      return $controller->$actionName();
+    }
+
+    // Do something to handle requests that do not map
+    // to existing controllers or actions.
+
+  }
+
 }
 ?>
