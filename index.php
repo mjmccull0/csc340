2fcaf6a9c068b975a9e11d529f8f01d47939ddb8
Michael McCulloch <m.mcculloch@gmail.com>
2018-09-18 14:59:13 -0400

    Removed from index.php which was never intended to stay in the file permanently.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @date 9/12/18
+ * @date 9/18/18
  * @author Michael McCulloch
  */
 
@@ -12,41 +12,8 @@ spl_autoload_register(
   }
 );
 
-// Creates a data source by providing a url to get data from
-// and providing a location to store the relevant information.
-$dataSource = WpDataSource::create(
-  array(
-    'path' => 'posts.txt',
-    'model' => 'WpModel',
-    'url' => 'https://newsandfeatures.uncg.edu/wp-json/wp/v2/posts?_embed'
-  )
-);
-$dataSource->import();
-// Creates a queriable data object.
-$wpData = TextDB::create($dataSource);
-
-var_dump($wpData);
-
-// The following will soon be removed.
-// Note: var_dump($blah) will dump the value of $blah.
-
-// This is an example of how to get a record with a given id.
-// var_dump($wpData->fetchById(36329));
-// var_dump($wpData->fetchById(36329)->getTitle());
-
-// This is an example of how to get all the records.
-var_dump($wpData->fetchAll());
-
-$instagramDataSource = InstagramDataSource::create(
-  array(
-    'path' => 'instagram.txt',
-    'model' => 'InstagramModel',
-    'url' => 'https://www.instagram.com/uncg/'
-  )
-);
-
-
-$instagramData = TextDB::create($instagramDataSource);
-var_dump($instagramData->fetchAll());
+// This is probably a temporary way to route an
+// appropriate controller and action.
+Util::getRoute($_SERVER['REQUEST_URI']);
 
 ?>
