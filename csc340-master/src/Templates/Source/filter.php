29b7eecf21d00c70e59c1e446ec2f391a1af1266
Jacob Oleson <jtoleson@uncg.edu>
2018-11-27 10:32:56 -0500

    Worked on the filter template and removed an extraneous file.

--- csc340-master/src/Templates/Source/filter.php
+++ csc340-master/src/Templates/Source/filter.php
@@ -1,19 +1,11 @@
 <!--
 view for search results
-@ Update 11/15/18
+@ Update 11/27/18
 @author Jacob Oleson
 -->
 
 <h1>Search Results:</h1>
-<?php
-var_dump('<pre>');
-var_dump($this->data);
 
-// foreach($this->data as $record) {
-//   echo $record->getTitle();
-//   echo "      ";
-// }
-?>
 
 <?php foreach ($this->data as $data) { ?>
 
@@ -22,8 +14,19 @@ var_dump($this->data);
   <?php } ?>
   <div>
     <span class="title"><?php echo $data->getTitle(); ?></span>
+    <br>
+
+    <span class="type"><?php echo "TYPE: " . $data->getType(); ?></span>
+    <br>
+
+    <b>Active</b>
+
+    <input type="checkbox" name="active" value="active" <?php if ($data->getActive()) echo 'checked' ; ?>>
   </div>
+
   <a href="<?php echo $this->baseUrl . EDIT . '?' . ID . '=' . $data->getId()   ?>">
     <?php echo EDIT ?></a>
+  <br>
 <?php } ?>
-</ul>
+
+<input class="button" type="submit" value="Save">
