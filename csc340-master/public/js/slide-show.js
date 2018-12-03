68c961c56c20e5d54e047a63336ce2e26386bd72
Jacob Oleson <jtoleson@uncg.edu>
2018-12-03 16:48:42 -0500

    Did I mention that I hate GitHub?

--- csc340-master/public/js/slide-show.js
+++
@@ -1,9 +0,0 @@
-let slides = document.querySelectorAll('#slides .slide');
-let index = 0;
-let slideInterval = setInterval(nextSlide,11000);
-
-function nextSlide() {
-    slides[index].className = slides[index].className.replace(/active/i, '');
-    index = (index+1) % slides.length;
-    slides[index].className = slides[index].className + ' active';
-}
--- csc340-master/src/Templates/Source/source.php
+++
@@ -1,9 +0,0 @@
-<div class="list-view-item">
-    <div class="source">
-      <span class="title"><?php echo $this->source->getName(); ?></span>
-    </div>
-    <a href="<?php echo $this->baseUrl . EDIT . '?' . NAME . '=' . $this->source->getName() ?>">
-      <?php echo EDIT ?></a>
-    <a href="<?php echo $this->baseUrl . SHOW . '?' . NAME . '=' . $this->source->getName(); ?>">
-      <?php echo SHOW ?></a>
-</div>
