80d352db80c828eec6551c14efab9b1691c8bb6e
Jacob Oleson <jtoleson@uncg.edu>
2018-12-03 16:48:42 -0500

    Did I mention that I hate GitHub?

--- csc340-master/src/Templates/Instagram/sourceEdit.php
+++
@@ -1,4 +0,0 @@
-Instagram Account:<br>
-<?php $account = explode('/', $this->data->getUrl())[3]; ?>
-<input type="text" name="instagram-account" value="<?php echo $account; ?>">
-<br>
--- csc340-master/src/Templates/Posts/sourceEdit.php
+++
@@ -1,6 +0,0 @@
-<?php $siteUrl = 'http://' .  parse_url($this->data->getUrl())['host']; ?>
-  <div id="Posts">
-    Site Url:<br>
-    <input type="text" name="wp-site-url" value="<?php echo $siteUrl; ?>">
-    <br>
-  </div>
